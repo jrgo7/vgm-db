8e5446835fd5fcc4788511bf91b69a108aadd6b6
Justin Rainier Go <justinrainiergo@gmail.com>
2024-11-27 11:29:59 +0800

    More search, sign-up transactions;

--- queries.sql
+++ queries.sql
@@ -1,48 +1,165 @@
--- In this SQL file, write (and comment!) the typical SQL queries users will run
--- on your database
-
 -- I. Listening-related queries
-
 -- Simple query for music from some application:
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20;
+SELECT
+    `title`
+FROM
+    `music`
+ORDER BY
+    `title`
+LIMIT
+    20;
 
 -- Simple query with pagination -- get the "next page" of results:
 -- OFFSET 20 means "start on the 20th result"
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20 OFFSET 20;
+SELECT
+    `title`
+FROM
+    `music`
+ORDER BY
+    `title`
+LIMIT
+    20
+OFFSET
+    20;
 
--- Filter by company: TODO
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20;
+-- Filter by company
+SELECT
+    `title`,
+    CONCAT (`composers`.`first_name`, `composers`.`last_name`) AS `composer`
+FROM
+    `music`
+    JOIN `credits` ON `music_id` = `credits_music_id`
+    JOIN `companies` ON `credits`.`company_id` = `companies`.`company_id`
+WHERE
+    `companies`.`name` LIKE "%Composer%"
+ORDER BY
+    `title`
+LIMIT
+    20;
 
--- Filter by composer: TODO
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20;
+-- Filter by composer
+SELECT
+    `title`,
+    CONCAT (`composers`.`first_name`, `composers`.`last_name`) AS `composer`
+FROM
+    `music`
+    JOIN `credits` ON `music_id` = `credits_music_id`
+    JOIN `composers` ON `credits`.`composer_id` = `composers`.`composer_id`
+WHERE
+    `composers`.`first_name` LIKE "%Composer%"
+    OR `composers`.`last_name` LIKE "%Composer%"
+ORDER BY
+    `title`
+LIMIT
+    20;
 
 -- Filter by game: TODO
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20;
+SELECT
+    `title`
+FROM
+    `music`
+ORDER BY
+    `title`
+LIMIT
+    20;
 
 -- Filter by game series: TODO
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20;
+SELECT
+    `title`
+FROM
+    `music`
+ORDER BY
+    `title`
+LIMIT
+    20;
 
 -- Filter by console: TODO
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20;
+SELECT
+    `title`
+FROM
+    `music`
+ORDER BY
+    `title`
+LIMIT
+    20;
 
 -- Filter by everything but game: TODO
-SELECT `title` FROM `music` ORDER BY `title` LIMIT 20;
+SELECT
+    `title`
+FROM
+    `music`
+ORDER BY
+    `title`
+LIMIT
+    20;
 
 -- II. Account-related queries
-
 -- Sign-up
+INSERT INTO
+    `accounts` (`username`, `email`, `password`, `type`)
+VALUES
+    (
+        "jrgo7",
+        "test_email@email.service.com",
+        SHA1 ("password1234"),
+        "user"
+    );
 
 -- Composer sign-up
+START TRANSACTION;
+
+INSERT INTO
+    `accounts` (`username`, `email`, `password`, `type`)
+VALUES
+    (
+        "yuzoko",
+        "yuzoko@test.com",
+        SHA1 ("actRaiser"),
+        "composer"
+    );
+
+INSERT INTO
+    `composers` (`account_id`, `last_name`, `first_name`)
+VALUES
+    (
+        SELECT
+            MAX(`account_id`)
+        FROM
+            `accounts`,
+            "Koshiro",
+            "Yuzo"
+    );
+    
+COMMIT;
 
 -- Company sign-up
+START TRANSACTION;
 
+INSERT INTO
+    `accounts` (`username`, `email`, `password`, `type`)
+VALUES
+    (
+        "pretendo",
+        "pretendo@pretendo.com",
+        SHA1 ("its-a-you"),
+        "company"
+    );
+
+INSERT INTO
+    `companies` (`account_id`, `name`)
+VALUES
+    (
+        SELECT
+            MAX(`account_id`)
+        FROM
+            `accounts`,
+            "Pretendo"
+    );
+
+COMMIT;
 
 -- III. Games, series, and consoles management
-
 -- Adding
-
 -- Editing
-
 -- Removing
-
 -- IV. Music management
--- schema.sql
+++ schema.sql
@@ -1,102 +1,121 @@
 -- In this SQL file, write (and comment!) the schema of your database, including
 -- the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
-
 -- Represents all accounts in the system.
-CREATE TABLE `accounts` (
-    `account_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
-    `username` VARCHAR(32) UNIQUE NOT NULL,
-    `email` VARCHAR(64) UNIQUE NOT NULL, 
-    `password` BINARY(20) NOT NULL, -- SHA-1
-    `type` ENUM('user', 'composer', 'company') NOT NULL,
-    `join_date` DATE DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE
+    `accounts` (
+        `account_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
+        `username` VARCHAR(32) UNIQUE NOT NULL,
+        `email` VARCHAR(64) UNIQUE NOT NULL,
+        `password` BINARY(20) NOT NULL, -- SHA-1
+        `type` ENUM ('user', 'composer', 'company') NOT NULL,
+        `join_date` DATE DEFAULT CURRENT_TIMESTAMP
+    );
 
 -- Biography entries take up a lot of space, and not everyone would probably
 -- have an entry, so they're separate from the `accounts` table.
-CREATE TABLE `bio` (
-    `account_id` INT UNSIGNED,
-    `bio` VARCHAR(2048) NOT NULL,
-    FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`)
-);
+CREATE TABLE
+    `bio` (
+        `account_id` INT UNSIGNED,
+        `bio` VARCHAR(2048) NOT NULL,
+        FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`)
+    );
 
 -- Unlike users, composers can provide additional information about themselves.
-CREATE TABLE `composers` (
-    `composer_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT
-    `last_name` VARCHAR(64) NOT NULL,
-    `middle_initial` VARCHAR(1), -- Not every name has a middle initial
-    `first_name` VARCHAR(64) NOT NULL
-);
+CREATE TABLE
+    `composers` (
+        `composer_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
+        `account_id` INT UNSIGNED,
+        -- Composers may or may not sign up for an account; companies could
+        -- publish their music on behalf of them instead.
+        `last_name` VARCHAR(64) NOT NULL,
+        `middle_initial` VARCHAR(1), -- Not every name has a middle initial
+        `first_name` VARCHAR(64) NOT NULL
+        FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`)
+    );
 
 -- Companies, like composers, can provide additional information.
-CREATE TABLE `companies` (
-    `company_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
-    `name` VARCHAR(64) NOT NULL UNIQUE
-);
+CREATE TABLE
+    `companies` (
+        `company_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
+        `account_id` INT UNSIGNED NOT NULL, -- Companies must sign up for an account.
+        `name` VARCHAR(64) NOT NULL UNIQUE
+        FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`)
+    );
 
 -- Represents video games, where VGM can appear in.
-CREATE TABLE `games` (
-    `game_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
-    `company_id` INT UNSIGNED,
-    `name` VARCHAR(64) NOT NULL UNIQUE,
-    FOREIGN KEY `company_id` REFERENCES `companies` (`company_id`)
-);
+CREATE TABLE
+    `games` (
+        `game_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
+        `company_id` INT UNSIGNED,
+        `name` VARCHAR(64) NOT NULL UNIQUE,
+        FOREIGN KEY `company_id` REFERENCES `companies` (`company_id`)
+    );
 
 -- Represents a series of related video games.
-CREATE TABLE `series` (
-    `series_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
-    `company_id` INT UNSIGNED,
-    `name` VARCHAR(64) NOT NULL UNIQUE,
-    FOREIGN KEY `company_id` REFERENCES `companies` (`company_id`)
-);
+CREATE TABLE
+    `series` (
+        `series_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
+        `company_id` INT UNSIGNED,
+        `name` VARCHAR(64) NOT NULL UNIQUE,
+        FOREIGN KEY `company_id` REFERENCES `companies` (`company_id`)
+    );
 
 -- Represents a video game console.
-CREATE TABLE `consoles` (
-    `console_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
-    `company_id` INT UNSIGNED,
-    `name` VARCHAR(64) NOT NULL UNIQUE
-    FOREIGN KEY `company_id` REFERENCES `companies` (`company_id`)
-);
+CREATE TABLE
+    `consoles` (
+        `console_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
+        `company_id` INT UNSIGNED,
+        `name` VARCHAR(64) NOT NULL UNIQUE FOREIGN KEY `company_id` REFERENCES `companies` (`company_id`)
+    );
 
 -- Associates games to series. Games can be part of many series,
 -- e.g., Mario Kart 8 is in the Mario Kart series and in the wider Mario series.
-CREATE TABLE `games_series` (
-    `game_id` INT UNSIGNED,
-    `series_id` INT UNSIGNED,
-    FOREIGN KEY `game_id` REFERENCES `games` (`game_id`),
-    FOREIGN KEY `series_id` REFERENCES `series` (`series_id`)
-);
+CREATE TABLE
+    `games_series` (
+        `game_id` INT UNSIGNED,
+        `series_id` INT UNSIGNED,
+        FOREIGN KEY `game_id` REFERENCES `games` (`game_id`),
+        FOREIGN KEY `series_id` REFERENCES `series` (`series_id`)
+    );
 
 -- Associates games to consoles. Games can be released in many consoles, such as
 -- Minecraft, which has been released for the PC, mobile phones, etc.
-CREATE TABLE `games_consoles` (
-    `game_id` INT UNSIGNED,
-    `series_id` INT UNSIGNED,
-    FOREIGN KEY `game_id` REFERENCES `games` (`game_id`),
-    FOREIGN KEY `series_id` REFERENCES `series` (`series_id`)
-);
+CREATE TABLE
+    `games_consoles` (
+        `game_id` INT UNSIGNED,
+        `series_id` INT UNSIGNED,
+        FOREIGN KEY `game_id` REFERENCES `games` (`game_id`),
+        FOREIGN KEY `series_id` REFERENCES `series` (`series_id`)
+    );
 
 -- Represents VGM.
-CREATE TABLE `music` (
-    `music_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
-    `title` VARCHAR(64) NOT NULL,
-    `path` VARCHAR(128), -- file path
-    `upload_date` DATE DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE
+    `music` (
+        `music_id` INT PRIMARY KEY UNSIGNED AUTOINCREMENT,
+        `title` VARCHAR(64) NOT NULL,
+        `path` VARCHAR(128), -- file path
+        `upload_date` DATE DEFAULT CURRENT_TIMESTAMP
+    );
 
 -- Tracks the credits of a certain song.
 -- A composer may not always be in the same company, so a company must also
 -- be provided.
-CREATE TABLE `credits` (
-    `music_id` INT UNSIGNED NOT NULL,
-    `company_id` INT UNSIGNED NOT NULL,
-    `composer_id` INT UNSIGNED NOT NULL,
-    FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`),
-    FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`),
-    FOREIGN KEY (`composer_id`) REFERENCES `composer` (`composer_id`)
-);
+CREATE TABLE
+    `credits` (
+        `music_id` INT UNSIGNED NOT NULL,
+        `company_id` INT UNSIGNED NOT NULL,
+        `composer_id` INT UNSIGNED NOT NULL,
+        FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`),
+        FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`),
+        FOREIGN KEY (`composer_id`) REFERENCES `composer` (`composer_id`)
+    );
 
 -- VGM can appear in multiple games; this tracks each game appearance of a VGM.
-CREATE TABLE `appearances` (
-    `music_id` INT UNSIGNED NOT NULL,
-    `game_id` INT UNSIGNED NOT NULL,
-);
+CREATE TABLE
+    `appearances` (
+        `music_id` INT UNSIGNED NOT NULL,
+        `game_id` INT UNSIGNED NOT NULL,
+    );
+
+-- Anticipating that people would search by title
+CREATE INDEX `music_title` ON `music` (`title`);
